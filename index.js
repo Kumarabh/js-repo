ac79c01491540534fa594ce714abc7dff3eb5111
Kumarabh <abhishek.km2010@gmail.com>
2020-07-19 23:37:42 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 import "./style.css";
 
 // Write Javascript code!
-document.getElementById('app').innerText = "https://github.com/Kumarabh/js-repo";
+document.getElementById('demo').innerText = "https://github.com/Kumarabh/js-repo";
