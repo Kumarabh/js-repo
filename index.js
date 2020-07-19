ebcb6711e2c3536fa8a2a3fe4fd6e8448b9468ad
Abhishek Kumar <abhishek.km2010@gmail.com>
2020-07-19 23:36:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 import "./style.css";
 
 // Write Javascript code!
-document.getElementById('app').innerHTML = "https://github.com/Kumarabh/js-repo";
+document.getElementById('app').innerText = "https://github.com/Kumarabh/js-repo";
